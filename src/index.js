910c98860bf6734c1117b4b19112b3950b2461cd
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2022-08-25 00:22:52 +0900

    initialized material ui

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createTheme,
+  ThemeProvider,
+} from '@mui/material/styles';
+import {
+  red,
+} from '@mui/material/colors';
 import '~/index.css';
 import { App } from '~/App';
+
+const theme = createTheme({
+  palette: {
+    warning: red,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider
+      theme={theme}
+    >
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
